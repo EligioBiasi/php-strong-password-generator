1ff60e2770dd98b416a920d4a77b1c13e02a327a
Eligio Biasi <ex_98@live.it>
2023-07-04 15:54:16 +0200

    added body background

--- index.php
+++ index.php
@@ -21,9 +21,10 @@ function generatePw(){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password generator</title>
+    <!-- bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <!-- my style -->
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href=".\style.css">
 </head>
     <body>
         <h1 class="text-center">
@@ -37,7 +38,7 @@ function generatePw(){
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
-        <div>
+        <div class="d-flex justify-content-center mt-2">
             <p>
                 La tua nuova password è: <?php echo generatePw() ?>
             </p>
